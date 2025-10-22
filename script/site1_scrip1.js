24c143aa4e2f726b73c8e93883996d1a5b44e6a7
larcel <2355315@cegepoutaouais.qc.ca>
2025-10-22 15:19:06 -0400

    type sh

--- script/site1_scrip1.js
+++ script/site1_scrip1.js
@@ -1,15 +1,37 @@
 const boutonCouleur = document.querySelectorAll(".boutonCouleur");
 const bigBoxGauche = document.querySelector(".BoiteGauche");
-
 //Création plusieurs boutons dans la boite Gauche
-for (let i = 0; i < 10; i++) {
+let id = 0
+for (let i = 0; i < 40; i++) {
     const cercle = document.createElement("p");
     const cadreCercle = document.createElement("div");
     cadreCercle.classList.add("BoxWithOneCercle");
     cercle.classList.add("CercleInBox");
-    cercle.style.background = "red";
+    cercle.id = `ID${i}`
     cadreCercle.append(cercle);
     bigBoxGauche.append(cadreCercle);
+}
+
+// put the border of the case cercle blue
+const cercleInvisibe = document.querySelectorAll(".CercleInBox");
+const caddre =document.querySelectorAll(".BoxWithOneCercle");
+const chaque_ligne_list = []
+caddre.forEach((leCadre) => {
+    if (!(chaque_ligne_list.length === 4)) {
+        chaque_ligne_list.push(leCadre);
+    }
+})
+chaque_ligne_list.forEach((leCadre) => {
+    leCadre.style.border = "2px solid blue";
+})
+let positionDansListe = 0
+function DonnerCouleurCercle(cercle){
+    const LeCadre  = chaque_ligne_list[positionDansListe]
+    const cercleInvisible = LeCadre.firstChild
+    if (cercleInvisible.style.backgroundColor === "") {
+        cercleInvisible.style.backgroundColor = cercle.style.backgroundColor ;
+        }
+    positionDansListe++
 }
 
 const liste_couleur = ['red',
@@ -27,17 +49,19 @@ const liste_couleur = ['red',
     'black',
     'white',
     'gray']
-// Assigner les couleurs au boutons
 
-boutonCouleur.forEach(function (bouton,index) {
-    bouton.style.backgroundColor = liste_couleur[index]
+// Assigner les couleurs au boutons
+boutonCouleur.forEach(function (boutonCercle,index) {
+    boutonCercle.style.backgroundColor = liste_couleur[index]
 
     //Activer lorsque tu click un boutton
-    bouton.addEventListener('click', function () {
-        bouton.style.opacity = 0.5;
+    boutonCercle.addEventListener('click', function () {
+        boutonCercle.style.opacity = 0.5;
         setTimeout(function () {
-            bouton.style.opacity = 1;
+            boutonCercle.style.opacity = 1;
+            DonnerCouleurCercle(boutonCercle)
         }
             ,1000)
     })
 });
+
